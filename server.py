cd1a4ca7f6d17f1d9b9a0c1b8232243f2eb8144a
Jen Montes <jen@jenmontes.com>
2014-03-30 01:54:58 -0400

    Get the correct settings file to display

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
+import os
 from eve import Eve
-app = Eve()
+
+settings_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py')
+
+app = Eve(settings=settings_file)
 
 if __name__ == '__main__':
     app.run()
