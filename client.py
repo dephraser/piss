399f5491da6fa4536c2594df8dbfa44121ba44d1
Jen Montes <jen@jenmontes.com>
2014-03-30 02:32:30 -0400

    Created a template and moved hard-coded values to a configuration file

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path = "")
+app = Flask(__name__, static_url_path = "", instance_relative_config=True)
+app.config.from_pyfile('piss.cfg', silent=True)
 
 @app.route('/')
 def get_index():
     # Redirect to an index.html page
-    return app.send_static_file('index.html')
+    return render_template('index.html')
     
 @app.after_request
 def process_response(response):
@@ -13,5 +14,4 @@ def process_response(response):
     return response
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
