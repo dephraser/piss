d95042656c12bf61913308e18bfcd3ab03f22cd3
Jen Montes <jen@jenmontes.com>
2014-08-02 19:29:13 -0400

    Added Content-Type detection back to dispatcher
    Using only 'Accept: text/html' to detect browsers seemed like a
    good idea, but that means that I would need to white-list every
    mime type that browsers would be expecting: images, css,
    javascript...
    
    It's much more sensible to make the HTML renderer the default
    app and only switch to the Eve backend if 'application/json' or
    'application/xml' are detected in either the 'Accept' or 'Content-
    Type' headers.

--- piss.py
+++ piss.py
@@ -2,9 +2,9 @@ from flask import Flask
 from data import app as data
 from html_renderer import app as html_renderer
 
-class AcceptTypeDispatcher(object):
+class DataTypeDispatcher(object):
     """
-    Mount application based on 'Accept' in the request header.
+    Mount application based on 'Accept' or 'Content-Type' in the request header.
     """
 
     def __init__(self, default_app, dispatched_app, content_types=None):
@@ -14,16 +14,17 @@ class AcceptTypeDispatcher(object):
 
     def __call__(self, environ, start_response):
         accept_type = environ.get('HTTP_ACCEPT', '').lower()
+        content_type = environ.get('CONTENT_TYPE', '').lower()
         app = self.default
         for dispatch_type in self.dispatch_types:
-            if dispatch_type in accept_type:
+            if accept_type.startswith(dispatch_type) or content_type.startswith(dispatch_type):
                 app = self.dispatched
                 break
         return app(environ, start_response)
 
 app = Flask(__name__)
 
-app.wsgi_app = AcceptTypeDispatcher(data, html_renderer, ['text/html'])
+app.wsgi_app = DataTypeDispatcher(html_renderer, data, ['application/json', 'application/xml'])
 
 if __name__ == '__main__':
     app.debug = True
