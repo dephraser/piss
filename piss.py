bcd4571124c02c217829ee9bb4f5a0929f5c7fb7
Jen Montes <jen@jenmontes.com>
2014-08-02 19:09:38 -0400

    Changed dispatcher to respond to 'Accept' headers instead of 'Content-Type'

--- piss.py
+++ piss.py
@@ -2,29 +2,28 @@ from flask import Flask
 from data import app as data
 from html_renderer import app as html_renderer
 
-class ContentTypeDispatcher(object):
+class AcceptTypeDispatcher(object):
     """
-    Mount application based on content type
+    Mount application based on 'Accept' in the request header.
     """
 
-    def __init__(self, base_app, dispatched_app, content_types=None):
-        self.base = base_app
+    def __init__(self, default_app, dispatched_app, content_types=None):
+        self.default = default_app
         self.dispatched = dispatched_app
         self.dispatch_types = content_types or []
 
     def __call__(self, environ, start_response):
-        content_type = environ.get('CONTENT_TYPE', '').lower()
-        app = self.base
+        accept_type = environ.get('HTTP_ACCEPT', '').lower()
+        app = self.default
         for dispatch_type in self.dispatch_types:
-            if content_type in dispatch_type:
+            if dispatch_type in accept_type:
                 app = self.dispatched
                 break
-        
         return app(environ, start_response)
 
 app = Flask(__name__)
 
-app.wsgi_app = ContentTypeDispatcher(data, html_renderer, ['text/html'])
+app.wsgi_app = AcceptTypeDispatcher(data, html_renderer, ['text/html'])
 
 if __name__ == '__main__':
     app.debug = True
