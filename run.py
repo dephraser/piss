880bf722f4af43ca31b24d1b5d98b01db84a9265
Jen Montes <jen@jenmontes.com>
2014-10-08 16:59:22 -0400

    Added validation for server configuration. Fixes #17

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 from eve import Eve
+from cerberus import Validator
 from piss.utils import NewBase60Encoder, NewBase60Validator
 from piss.auth import HawkAuth
 from piss.event_hooks import before_posts_insert, before_posts_update, before_posts_get, before_posts_post, after_posts_post
@@ -37,6 +39,23 @@ for setting in ('META_POST', 'ROOT_CREDENTIALS', 'SECRET_KEY', 'MENU_ITEMS', 'SE
         missing_settings.append(setting)
 if missing_settings:
     raise SystemExit('Missing configuration settings! (%s)' % (','.join(missing_settings),))
+
+# Check that a `meta.json` file exists in `types` and that you can read from it
+meta_schema = ''
+meta_schema_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'types/meta.json')
+try:
+    with open(meta_schema_file, 'r') as f:
+        meta_schema = f.read()
+except IOError as e:
+    raise SystemExit('Could not find `meta` post schema file at %s!' % (meta_schema_file,))
+if not meta_schema:
+    raise SystemExit('No data in `meta` post schema at %s!' % (meta_schema_file,))
+
+# Validate the data in `META_POST` against the `meta` post schema
+v = Validator(json.loads(meta_schema))
+if not v.validate(app.config['META_POST']):
+    raise SystemExit('Invalid `META_POST` configuration! \
+        Validator returned the following errors: \n%s' % (str(v.errors),))
 
 # Make sure necessary directories exist
 if not os.path.isdir(os.path.join(app.instance_path, 'attachments')):
